02dd15a4ba57f0d60e1c165862dd24b7741f168a
Endairion <86313950+Endairion@users.noreply.github.com>
2022-10-24 17:29:55 +0800

    Descriptive Analysis

--- BigDataCode.R
+++ BigDataCode.R
@@ -52,3 +52,60 @@ ggplot(m_system.time) + aes(x = apply_function, y = user.self, label = round(use
   ) + 
   scale_fill_manual(values = v_color)
 #Parallel and Sequential Processing----------------------------------------------------------------------------------------
+
+#Descriptive Analysis============================================================
+#reading from csv
+df <- read.csv("DiabetesDataset.csv")
+
+#check if any NA value
+is.na(df)
+#structure of df
+str(df)
+#summary of df
+summary(df, digits = 5)
+
+#create new dataframe that store 3 attributes without the area_id
+grouped_df <- select(df,"sugar","carb","estimated_diabetes_prevalence")
+
+#calculate standard deviation, mean and median with the new dataframe
+sd <- lapply(grouped_df, sd)
+mean <- lapply(grouped_df, mean)
+median <- lapply(grouped_df, median)
+
+#bind the 3 values into a new dataframe
+new_df <- cbind(sd,mean,median)
+#Adding a column using the name of the row
+temp <- new_df
+category <- rownames(new_df)
+#setting the row name into default (number:1,2,3...)
+rownames(temp)<-NULL
+new_df<-cbind(category,temp)
+new_df<-as.data.frame(new_df)
+#unlist the data
+new_df$category<-unlist(new_df$category)
+new_df[2:4]<-as.numeric(unlist(new_df[2:4]))
+new_df
+
+#Visualization
+#plotting histogram for each category with mean and median line 
+hist(df$estimated_diabetes_prevalence)
+abline(v=median$estimated_diabetes_prevalence,col="blue",lwd=3,lty=2)
+abline(v=mean$estimated_diabetes_prevalence,col="red",lwd=3,lty=2)
+
+hist(df$sugar)
+abline(v=median$sugar,col="blue",lwd=3,lty=2)
+abline(v=mean$sugar,col="red",lwd=3,lty=2)
+
+hist(df$carb)
+abline(v=median$carb,col="blue",lwd=3,lty=2)
+abline(v=mean$carb,col="red",lwd=3,lty=2)
+
+
+
+#plotting mean and standard deviation for the data
+library(ggplot2)
+ggplot(new_df, aes(x=category, y=mean)) +
+  geom_errorbar(aes(ymin=mean-sd,ymax=mean+sd), width= .1)+
+  geom_point()
+
+#Descriptive Analysis============================================================
